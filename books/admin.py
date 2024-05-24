ca8807210bca299c3abbbc5d07d425c23e31feed
ahmedmt3 <85899361+ahmedmt3@users.noreply.github.com>
2024-05-24 20:52:25 +0300

    Last changes

--- books/admin.py
+++ books/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'borrowed', 'category']
+    list_editable = ['borrowed', 'category']
+    
+admin.site.register(Book, BookAdmin)
--- books/urls.py
+++ books/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.books, name='books'),
     path('book', views.book, name='book'),
-    path('/borrowed', views.borrowed, name='borrowed')
+    path('borrowed', views.borrowed, name='borrowed')
 ]
